89f4db60f25206eb533d0d8f6155be98772f3580
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2025-11-22 23:45:44 +0900

    PlayerがMaxじゃなかったらBOT生成

--- Assets/Scripts/Joint/MainGameManger.cs
+++ Assets/Scripts/Joint/MainGameManger.cs
@@ -1,3 +1,4 @@
+using Unity.Entities;
 using Unity.Services.Authentication;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,9 +8,11 @@ using UnityEngine.UIElements;
 public class MainGameManger : MonoBehaviour
 {
     [SerializeField] private GameObject[] playerPrefab = default; //Player
+    [SerializeField] private GameObject botPrefab = default;
 
     [SerializeField] private Transform[] pos = default;         //生成位置
- 
+
+  
     void Start()
     {
         //インスタンスがない場合はreturn
@@ -19,44 +22,31 @@ public class MainGameManger : MonoBehaviour
         var devices = PlayerDataHolder.Instance.GetDevices();
         int count   = PlayerDataHolder.Instance.GetPlayerCount();
 
+        //------------
+  
+              
+        
+
+
         //人数分Playerの生成,PlayerID
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < pos.Length; i++)
         {
-            
-           
-
-            // 指定デバイスで PlayerInput を持つプレイヤーを生成
-            var obj = PlayerInput.Instantiate(
-                prefab: playerPrefab[i],
-                playerIndex: i,
-                pairWithDevice: devices[i]
-             );
-            //生成後この位置にセット
-            obj.transform.position = pos[i].position;
-            obj.transform.rotation = pos[i].rotation;
-
-            //数が表示される
-  /*          var ui = obj.GetComponent<PlayerUI>();
-            if (devices[i] == null)
+            if(i <  count && devices[i] != null)
             {
-                return;
+                // 指定デバイスで PlayerInput を持つプレイヤーを生成
+                var obj = PlayerInput.Instantiate(
+                    prefab: playerPrefab[i],
+                    playerIndex: i,
+                    pairWithDevice: devices[i]
+                 );
+                //生成後この位置にセット
+                obj.transform.position = pos[i].position;
+                obj.transform.rotation = pos[i].rotation;
             }
-            if (devices[0] != null)
+            else
             {
-                ui.SetCount(1);
+                Instantiate(botPrefab, pos[i].position, pos[i].rotation);
             }
-            if (devices[1] != null)
-            {
-                ui.SetCount(2);
-            }
-            if (devices[2] != null)
-            {
-                ui.SetCount(3);
-            }
-            if (devices[3] != null)
-            {
-                ui.SetCount(4);
-            }*/
         }
 
        
